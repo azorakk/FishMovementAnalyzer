94b5970cbfd6851dadc196bdbab6d60bdbb96db0
azorakk <154017575+azorakk@users.noreply.github.com>
2024-01-13 13:12:45 +0100

    export data to excel

--- FishMovementAnalyzerApp/Form1.Designer.cs
+++ FishMovementAnalyzerApp/Form1.Designer.cs
@@ -30,7 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.DragAndDrop = new System.Windows.Forms.Label();
-            this.Message = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.messagelbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // DragAndDrop
@@ -38,32 +40,48 @@
             this.DragAndDrop.AllowDrop = true;
             this.DragAndDrop.Image = ((System.Drawing.Image)(resources.GetObject("DragAndDrop.Image")));
             this.DragAndDrop.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.DragAndDrop.Location = new System.Drawing.Point(214, 87);
+            this.DragAndDrop.Location = new System.Drawing.Point(244, 95);
             this.DragAndDrop.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
             this.DragAndDrop.Name = "DragAndDrop";
-            this.DragAndDrop.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
-            this.DragAndDrop.Size = new System.Drawing.Size(259, 129);
+            this.DragAndDrop.Padding = new System.Windows.Forms.Padding(0, 0, 0, 13);
+            this.DragAndDrop.Size = new System.Drawing.Size(296, 172);
             this.DragAndDrop.TabIndex = 0;
-            this.DragAndDrop.Text = "Drag And Drop Files";
+            this.DragAndDrop.Text = "Drag And Drop CSV File";
             this.DragAndDrop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Message
+            // backgroundWorker
             // 
-            this.Message.Location = new System.Drawing.Point(182, 279);
-            this.Message.Name = "Message";
-            this.Message.Size = new System.Drawing.Size(346, 23);
-            this.Message.TabIndex = 1;
-            this.Message.Text = "Message";
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(110, 363);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(589, 52);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
+            // messagelbl
+            // 
+            this.messagelbl.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.messagelbl.Location = new System.Drawing.Point(110, 281);
+            this.messagelbl.Name = "messagelbl";
+            this.messagelbl.Size = new System.Drawing.Size(589, 66);
+            this.messagelbl.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(700, 338);
-            this.Controls.Add(this.Message);
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.messagelbl);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.DragAndDrop);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Fish Movement Analyzer";
             this.ResumeLayout(false);
@@ -73,6 +91,8 @@
         #endregion
 
         private Label DragAndDrop;
-        private Label Message;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private ProgressBar progressBar1;
+        private Label messagelbl;
     }
 }
